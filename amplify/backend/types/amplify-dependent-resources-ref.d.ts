bed01a5aeb3101072cc47afe5b950baea5aec37c
khang <enone859@gmail.com>
2024-02-13 21:34:42 -0500

    prod configs

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,6 +1,6 @@
 export type AmplifyDependentResourcesAttributes = {
   "auth": {
-    "wothtoolboxawsamplifa2a29ca8": {
+    "wothtoolboxawsamplifd33dce73": {
       "AppClientID": "string",
       "AppClientIDWeb": "string",
       "IdentityPoolId": "string",
